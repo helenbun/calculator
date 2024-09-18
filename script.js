c5f43b787dcb6b014d56505c7d85807d4f60e31a
Helen <180019665+helenbun@users.noreply.github.com>
2024-09-18 08:44:58 +0100

    Add HTML and CSS to make calculator appear
    Also a basic javascript function to evaluate two numbers and an operator.

---
+++ script.js
@@ -0,0 +1,22 @@
+let operator = null;
+let num1 = 0;
+let num2 = 0;
+
+function evaluate (num1,num2,operator) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+            break;
+        case "-":
+            return num1 - num2;
+            break;
+        case "*":
+            return num1 * num2;
+            break;
+        case "/":
+            return num1 / num2; 
+}
+}
+
+/*Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a 
+variable somewhere for use in the next step*/
